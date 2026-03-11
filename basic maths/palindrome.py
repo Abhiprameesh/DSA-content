39f9fd8b4bd1da11ca73fe3084acc13b9cd20276
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-03-11 22:23:53 +0530

    done

---
+++ basic maths/palindrome.py
@@ -0,0 +1,4 @@
+#A palindrome is a number that reads the same backward as forward. For example, 121, 1331, and 4554 are palindromes because they remain the same when their digits are reversed.
+n = input("Enter a value: ")
+
+s
