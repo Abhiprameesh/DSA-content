5b3720309ad38eae14a02e4846a3a7c60909950e
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-03-12 20:03:15 +0530

    done

--- basic maths/Armstrong.py
+++ basic maths/Armstrong.py
@@ -1,1 +1,13 @@
 #An Amrstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+number = int(input("Enter a number: "))
+length = len(str(number))
+sum = 0
+temp = number
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** length
+    temp //= 10
+if number == sum:
+    print(number, "is an Armstrong number.")
+else:
+    print(number, "is not an Armstrong number.")
