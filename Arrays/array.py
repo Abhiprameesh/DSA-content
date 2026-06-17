6a6315f90f8fcb552b8ee405be6cd393983edc1c
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-06-17 14:53:19 +0530

    done

---
+++ Arrays/array.py
@@ -0,0 +1,8 @@
+import array
+
+my_array = array.array('i', [1, 2, 3, 4, 5])
+print(my_array)  # Output: array('i', [1, 2, 3, 4, 5])
+
+import numpy as np
+my_numpy_array = np.array([1, 2, 3, 4, 5])
+print(my_numpy_array)  # Output: [1 2 3 4 5]
