0a463706b96850f27b580074589ba075f3da5da8
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-06-20 17:35:56 +0530

    done

--- Arrays/TwoDimensionalArray.py
+++ Arrays/TwoDimensionalArray.py
@@ -7,9 +7,9 @@ import numpy as np
 
 twoDarray = np.array([[11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9]])
 print(twoDarray)
-
+#insert a new column at the start of the array
 newTwoDArray = np.insert(twoDarray, 0, [[1,2,3,4]], axis=1)
 print(newTwoDArray)
-
+#insert a new row at the start of the array
 newTwoDArray = np.insert(twoDarray, 1, [[1,2,3,4]], axis=0)
 print(newTwoDArray)
