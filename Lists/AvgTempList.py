e98f2cbfd34c992551650caadcc5470f97c509a6
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-06-28 21:34:13 +0530

    finds the days above average temperature in the AvgTempList.py

---
+++ Lists/AvgTempList.py
@@ -0,0 +1,16 @@
+numDays = int(input("Enter the number of days: "))
+total = 0
+temp = [] 
+for i in range( numDays ):
+    nextDay = int(input("Day" + str(i + 1) + "'s high temp: "))
+    temp.append(nextDay)
+    total += temp[i]
+
+avg = round(total / numDays, 2)
+print("\nAverage = " + str(avg))
+
+above = 0 
+for i in temp:
+    if i > avg:
+        above += 1
+print(str(above) + " days were above average.")
