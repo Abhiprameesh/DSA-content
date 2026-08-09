52ac38616acbaaf97e400e04ae80ae325eb56ee7
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-08-09 21:06:56 +0530

    patterns

---
+++ Patterns/17th.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pattern2(self, n):
+
+        for i in range(n):
+
+            # Spaces
+            for j in range(n - i - 1):
+                print(" ", end="")
+
+            # Increasing letters
+            for j in range(i + 1):
+                print(chr(65 + j), end="")
+
+            # Decreasing letters
+            for j in range(i - 1, -1, -1):
+                print(chr(65 + j), end="")
+
+            print()
