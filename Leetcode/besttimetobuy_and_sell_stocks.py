6136a1f7b6481e0a3a1126da3251b7e16ffd9a16
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-08-30 12:26:13 +0530

    solving leetcode problems again here

---
+++ Leetcode/besttimetobuy_and_sell_stocks.py
@@ -0,0 +1,10 @@
+prices = [7, 1, 5, 3, 6, 4]
+max_profit = 0 
+min_price = float('inf')
+for price in prices:
+    if price < min_price:
+        min_price = price
+    profit = price - min_price
+    if profit > max_profit:
+        max_profit = profit
+return max_profit
---
+++ Leetcode/missingnumber.py
@@ -0,0 +1,6 @@
+nums = [3, 0, 1]
+
+n = len(nums)
+expected_sum = n * (n + 1) // 2
+actual_sum = sum(nums)
+return expected_sum - actual_sum
