8e7e13d229517c9229207b46c2e8113326e3d40a
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-08-30 13:17:02 +0530

    solving leetcode problems again here

--- Leetcode/besttimetobuy_and_sell_stocks.py
+++ Leetcode/besttimetobuy_and_sell_stocks.py
@@ -8,3 +8,11 @@ for price in prices:
     if profit > max_profit:
         max_profit = profit
 return max_profit
+
+#  max_profit = 0
+#         min_price = float("inf")
+#         n = len(prices)
+#         for i in range(0, n):
+#             min_price = min(min_price, prices[i])
+#             max_profit = max(max_profit, prices[i] - min_price)
+#         return max_profit
